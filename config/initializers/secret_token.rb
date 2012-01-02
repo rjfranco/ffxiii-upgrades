b62fefbe419b21d69f5ae57fab2034bf302389ea
Ramiro Jr. Franco <rjfranco@gmail.com>
2012-01-02 14:45:03 -0800

    Adding initial code generated by Rails for project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FfxiiiWeaponUpgradeCalculator::Application.config.secret_token = 'f5af1bf1306d39ccbd913051b288d84eba73684e3aee9c1ec4b46ba822b4676f31cdfa878f0b6daba341c493c35fac15e1a8a680f9bf1498775766f2ba87088e'
